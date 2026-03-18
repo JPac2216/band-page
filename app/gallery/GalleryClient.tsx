1572d8ad535da504ccae8870062efbe4bbc6ad20
JPac2216 <jakepaccione@gmail.com>
2026-03-18 17:17:34 -0400

    Updated gallery fixed shows

--- app/gallery/GalleryClient.tsx
+++ app/gallery/GalleryClient.tsx
@@ -26,9 +26,16 @@ export default function Gallery() {
                 <div className="relative flex flex-col md:flex-row items-center justify-start pt-0 gap-4 z-10">
                     <div className="flex flex-col">
                         <h1 className="text-xl font-semibold tracking-tight text-white leading-tight drop-shadow-[0_0_20px_rgba(255,255,255,0.35)]">
+                          LIVE @ Prudential Center, Newark, NJ | 3/11/26</h1>
+                        <YouTubeEmbed videoid="wGBA5Q-aO8g" height={300} width={430} />
+                    </div>
+                    <div className="flex flex-col">
+                        <h1 className="text-xl font-semibold tracking-tight text-white leading-tight drop-shadow-[0_0_20px_rgba(255,255,255,0.35)]">
                           LIVE @ Bravo's Cafe, East Orange, NJ | 11/29/25</h1>
                         <YouTubeEmbed videoid="AKpjSQEntGE" height={300} width={430} />
                     </div>
+                </div>
+                <div className="relative flex flex-col md:flex-row items-center justify-start pt-0 gap-4 z-10">
                     <div className="flex flex-col">
                         <h1 className="text-xl font-semibold tracking-tight text-white leading-tight drop-shadow-[0_0_20px_rgba(255,255,255,0.35)]">
                           LIVE @ The Meatlocker, Montclair, NJ | 1/19/25</h1>
